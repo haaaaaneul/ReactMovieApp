710eafd2e06ae69436a5abd74f94b348e6c3dc34
HANEUL <gksmf@hansung.ac.kr>
2019-05-09 19:04:56 +0900

    Practicing setState
    Practicing setState

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const movies = [
   },
   {
     title : "Full Metal Jacket",
-    poster : "https://cdn.shopify.com/s/files/1/0784/1125/products/Full_Metal_Jacket_1024x.jpg?v=1530145558"
+    poster : "https://cdn.shopify.com/s/files/1/0784/1125/products/Full_Metal_Jacket_1024x.jpg?v=1530145558"   
   },
   {
     title : "Oldboy",
@@ -23,16 +23,50 @@ const movies = [
   }
 ]
 
+
 class App extends Component{
+/*
+  componentWillMount() {
+    console.log('will mount'); //api 작업 요청, 사이클 시작
+  }
+*/
+
+//컴포넌트 안에 state가 바뀔 때 마다, render 작동
+  componentDidMount() {
+  //  console.log('did mount'); //데어터 관련 작업, 컴포넌트 자리잡았음
+    setTimeout(() => {
+      //state는 직접적으로 쓰면 안됨, this.state.~ = ~~ (X) , 직접접으로 변경하면 render 설정들이 작동 안함
+      this.setState({
+        greeting : 'Hello again' //컴포넌트가 mount 할 때마다, greeting을 Hello --> Hello again으로 변경함 
+        //greeting 렌더링을 setState에서 함
+      })
+    }, 5000)
+  }
+
+
+  state = {
+    greeting: 'Hello!'
+  }
+
   render() {
+   /* console.log('did render'); //컴포넌트 존재 */
+
     return (
       <div className="App">
-        {movies.map(movie => { //리스트
-          return <Movie title={movie.title} poster={movie.poster} /> //movies array를 가져다가 맵핑해서 새로운 array를 만듬
+        {this.state.greeting}
+        {movies.map((movie, index) => { //리스트
+          return <Movie title={movie.title} poster={movie.poster} key={index}/> //movies array를 가져다가 맵핑해서 새로운 array를 만듬
         })}
       </div>
     );
   }
 }
+
+/* Render : componentWillMount() -> render() -> componentDidMount()
+  will mount, render, did mount 3가지는 component가 존재하기 시작할때 작동 함 */
+
+  /* Update : componentWillReceiveProps() -> shouldComponentUpdate() 이전props과 새로운 props가 다르면 == true-> componentWillUpdate() -> render() -> componentDidUpdate()
+  willUpdate()때 spinner(로딩중)를 붙이고 업데이트 이후 DidUpdate()에서 메세지나 아이콘을 숨김 */
+
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,17 @@
 import React, { Component  } from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
 class Movie extends Component{
+
+
+static propTypes = {
+    title : PropTypes.string.isRequired, //string값을 가져야 한다 ? string만 출력? --> 참이 아니면 오류메세지 출력
+    poster : PropTypes.string.isRequired //isRequired라고 작성하면 movie컴포넌트는 poster라는 prop 제공하는 것이 필수로 설정 됨 --> 참이 아니면 오류 메세지
+    //부모 컴포넌트에서 받는 정보의 타입, 있는지 없는지 체크 가능
+
+}
+
     render() {
        // console.log(this.props);
         return (
@@ -14,6 +24,11 @@ class Movie extends Component{
 }
 
 class MoviePoster extends Component{
+
+    static propTypes = {
+        poster : PropTypes.string.isRequired //부모 컴포넌트에게 받는 정보 체크
+    }
+
     render() {
         console.log(this.props);
         return(
